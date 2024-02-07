394f20a0bce880d949dc9c9ff0f632aa5a42ff6b
Xevhra <garciarodriguezalvaro12@gmail.com>
2024-02-07 19:37:31 +0100

    añadir lineas de pedido

--- js/practicaObligatoria.js
+++ js/practicaObligatoria.js
@@ -48,16 +48,59 @@ const categorias = ["Aceite", "Encurtidos", "Salsas"];
 
 document.addEventListener("DOMContentLoaded", cargarDatos);
 document.getElementsByName("comerciales")[0].addEventListener("change", seleccionarCliente)
+document.getElementsByName("categorias")[0].addEventListener("change", seleccionarCategoria)
+document.getElementById("teclado").addEventListener("click", teclado)
+
+
+
+
 const gestor = new Gestor()
+const catalogo = new Catalogo()
 
 gestor.categorias = categorias;
 gestor.comerciales= comerciales;
 
-console.log(gestor);
 
 
 
 function cargarDatos() {
+  // Añadimos los productos
+  catalogo.addProducto(1, "Aceite Oliva Virgen Extra 1l (Caja 20)", 178.15, 0);
+  catalogo.addProducto(
+    2,
+    "Aceite Oliva Virgen Extra 700ml (Caja 30)",
+    208.5,
+    0
+  );
+  catalogo.addProducto(3, "Aceite Oliva Virgen Extra 5l (Caja 6)", 247.5, 0);
+  catalogo.addProducto(4, "Aceite Oliva 1l (Caja 20)", 109.25, 0);
+  catalogo.addProducto(5, "Aceituna Gordal 340gr (Caja de 50)", 180.75, 1);
+  catalogo.addProducto(
+    6,
+    "Aceituna Gordal deshuesada 350gr (Caja de 50)",
+    205.45,
+    1
+  );
+  catalogo.addProducto(7, "Aceituna Manzanilla 250 gr (Caja de 50)", 124.85, 1);
+  catalogo.addProducto(
+    8,
+    "Aceituna Manzanilla deshuesada 250 gr (Caja de 50)",
+    141.35,
+    1
+  );
+  catalogo.addProducto(9, "Aceituna Negra 350gr (Caja de 50)", 87.5, 1);
+  catalogo.addProducto(
+    10,
+    "Aceituna Negra deshuesada 350gr (Caja de 50)",
+    99.35,
+    1
+  );
+  catalogo.addProducto(11, "Mayonesa 350gr (Caja de 50)", 124.45, 2);
+  catalogo.addProducto(12, "Mayonesa 1Kg (Caja de 30)", 178.65, 2);
+  catalogo.addProducto(13, "Salsa Cocktail 350gr (Caja de 50)", 99.65, 2);
+  catalogo.addProducto(14, "Salsa Gaucha 350gr (Caja de 50)", 124.85, 2);
+  catalogo.addProducto(15, "Salsa Alioli 350 gr (Caja de 50)", 113.75, 2);
+  catalogo.addProducto(16, "Salsa Barbacoa 500gr (Caja de 30)", 67.5, 2);
   // Creamos el objeto gestor y lo instanciamos
   for (let com = 0; com < gestor.comerciales.length;com++) {
     gestor.clientes[com] = [];
@@ -68,14 +111,11 @@ function cargarDatos() {
     } 
   }
 
-  // Añadimos las categorias
-  console.log(categorias);
+
+
+  // Aqui asigno el array de categorias a gestor.categorias para luego poder cargarlas en el combobox
   
-  for (let cat = 0; cat < gestor.categorias.length; cat++) {
-    console.log(categorias);
-    gestor.categorias[cat] = []  
-    gestor.categorias[cat].push(categorias[cat]);
-  }
+  gestor.categorias = categorias;
 
   // Cargamos los comerciales en el select
 
@@ -95,48 +135,134 @@ function cargarDatos() {
     divCliente.classList.add('cliente');
     divCliente.classList.add('pagado');
     divCliente.addEventListener('click', seleccionarCliente);
+    divCliente.addEventListener('click', realizaPedido);
     divCliente.innerHTML = cliente.nombre;
     formComercial.after(divCliente);
   }  
 
 
-  // Cargamos las categorias en el select
-  console.log('Hola Piero');
+  // Cargamos las categorias y los productos en los select
   let selectCategorias = document.getElementsByName("categorias")[0];
-  for (let categoria of gestor.categorias){
-    opcionCategoria = document.createElement('option');
-    opcionCategoria.value = categoria;
-    opcionCategoria.append(document.createTextNode(categoria))
-    selectCategorias.append(opcionCategoria);
+  let selectProducto = document.getElementsByName("productos")[0]
+      for (let categoria of gestor.categorias){
+        opcionCategoria = document.createElement('option');
+        opcionCategoria.value = categoria;
+        opcionCategoria.setAttribute("value", gestor.categorias.indexOf(categoria))
+        opcionSeleccionada = document.getElementsByName("categorias")[0].value;
+        
+        opcionCategoria.append(document.createTextNode(categoria))
+        selectCategorias.append(opcionCategoria);
+      }
+// Añadimos los productos
+for (producto of catalogo.productos) {
+  if (producto.idCategoria == opcionSeleccionada) {
+    opcionProducto = document.createElement('option');
+    opcionProducto.value = producto;
+    opcionProducto.setAttribute("value", catalogo.productos.indexOf(producto));
+    opcionProducSeleccionada = document.getElementsByName("productos")[0].value;
+    opcionProducto.append(document.createTextNode(producto.nombreProducto));
+    selectProducto.append(opcionProducto); 
   }
+}
+
+
 }
 
 function seleccionarCliente () {
   let comercialSeleccionado = document.getElementsByName("comerciales")[0].value;
   gestor.comercialActual = gestor.comerciales.indexOf(comercialSeleccionado);
-  
   actualizaDatos();
+}
+
+
+function seleccionarCategoria () {
+  let categoriaSeleccionada = document.getElementsByName("categorias")[0].value;
+  
+  actualizaProductos();
 }
 
 
 function actualizaDatos() {
     borrarClientes();
-    console.log(('Hola Piero'));
     let clientes = document.getElementById("clientes");
     let clientesActuales = gestor.clientes[gestor.comercialActual];
-    console.log(gestor.comercialActual);
         for (let cli = 0; cli < clientesActuales.length; cli++) {
-            console.log('Adios Piero');
+            
             let cliente = clientesActuales[cli];
             let divCliente = document.createElement('div');
             divCliente.classList.add('cliente');
             divCliente.classList.add('pagado');
             divCliente.addEventListener('click', seleccionarCliente);
+            divCliente.addEventListener('click', realizaPedido);
             divCliente.innerHTML = cliente.nombre;
-            console.log(divCliente.innerHTML);
             clientes.appendChild(divCliente);
         }
+}
+
+function actualizaProductos() {
+  borrarOptions();
+  let selectProducto = document.getElementsByName("productos")[0]
+  let categorias = document.getElementsByName("categorias")[0];
+  let categoriaActual = categorias.value;
+  switch (categoriaActual) {
+    case '0':
+      
+      for (producto of catalogo.productos) {
+      if (producto.idCategoria == categoriaActual) {
+        opcionProducto = document.createElement('option');
+        opcionProducto.value = producto;
+        opcionProducto.setAttribute("value", catalogo.productos.indexOf(producto));
+
+        opcionProducSeleccionada = document.getElementsByName("productos")[0].value;
+        
+        opcionProducto.append(document.createTextNode(producto.nombreProducto));
+        selectProducto.append(opcionProducto); 
+      }
+}
+      break;
+
+    case '1':
+      
+      for (producto of catalogo.productos) {
+        if (producto.idCategoria == categoriaActual) {
+          opcionProducto = document.createElement('option');
+          opcionProducto.value = producto;
+          opcionProducto.setAttribute("value", catalogo.productos.indexOf(producto));
+          opcionProducSeleccionada = document.getElementsByName("productos")[0].value;
+  
+          
+          opcionProducto.append(document.createTextNode(producto.nombreProducto));
+          selectProducto.append(opcionProducto); 
+        }
+      }
+
+      break;
+      
+    case '2':
+      
+      for (producto of catalogo.productos) {
+      if (producto.idCategoria == categoriaActual) {
+        opcionProducto = document.createElement('option');
+        opcionProducto.value = producto;
+        opcionProducSeleccionada = document.getElementsByName("productos")[0].value;
+
+        
+        opcionProducto.append(document.createTextNode(producto.nombreProducto));
+        selectProducto.append(opcionProducto); 
+      }
+    }
+      break;  
+  }
+
+}
+
+function borrarOptions() {
+  let optionsABorrar = document.querySelectorAll('[name="productos"] option');
+  
+  for (let opcion of optionsABorrar){
     
+    opcion.remove();
+  }
 }
 
 
@@ -147,6 +273,35 @@ function borrarClientes() {
   }
 }
 
+
+// Funciones de pedidos
+
+
+let pedido = document.getElementById("pedido");
+
+function realizaPedido(event){
+  borrarDivPedido()
+  let clienteSeleccionado = event.target;
+  let divPedido = document.createElement('div');
+  divPedido.setAttribute("name", "titulo");
+  
+  divPedido.innerHTML = "<h2>"+clienteSeleccionado.innerHTML+"</h2>"
+  pedido.append(divPedido);
+  
+}
+
+function borrarDivPedido() {
+ let tituloABorrar = document.querySelectorAll('[name="titulo"]');
+ for (let titulo of tituloABorrar) {
+  titulo.remove();
+ }
+}
+
+
+function teclado(event) {
+  objetivo = event.target;
+  console.log(objetivo.value);
+}
 
 
 
